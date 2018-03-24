376c6be641bc1e81b6373b55c1f3a7fb06181cd0
PinHoWang <abc8755987@gmail.com>
2018-03-24 21:50:09 +0800

    Lecture 12: Finished code.

---
+++ week5/lec12/ug/pinho/animal/DogLauncher.java
@@ -0,0 +1,12 @@
+package ug.pinho.animal;
+
+/** 
+ *  @author Josh Hug
+ */
+
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog d = new Dog("frankie", "barnacle dog", 22);
+        System.out.println(d.toString());
+    }
+} 
