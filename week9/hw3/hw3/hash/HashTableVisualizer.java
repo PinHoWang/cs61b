2de21bae7076a7a83ac0792e0756aa783c22ee58
PinHoWang <abc8755987@gmail.com>
2018-05-22 23:59:03 +0800

    Homework 3: Finished HashTable Visualizer. Next: Complex Oomage.

--- week9/hw3/hw3/hash/HashTableVisualizer.java
+++ week9/hw3/hw3/hash/HashTableVisualizer.java
@@ -2,6 +2,7 @@ package hw3.hash;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Arrays;
 
 public class HashTableVisualizer {
 
@@ -29,6 +30,14 @@ public class HashTableVisualizer {
            du.xCoord and du.yCoord to figure out where to draw
            Oomages.
          */
+        int[] count = new int[M];
+        Arrays.fill(count, 1);
+
+        for(Oomage o : set) {
+          int code = (o.hashCode() & 0x7FFFFFFF) % M;
+          o.draw(HashTableDrawingUtility.xCoord(count[code]), HashTableDrawingUtility.yCoord(code, M), scale);
+          count[code]++;
+        }
 
         /* When done with visualizer, be sure to try 
            scale = 0.5, N = 2000, M = 100. */           
