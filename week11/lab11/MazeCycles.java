664c8ce338655ce6296683be04e40c5834f2b8bb
PinHoWang <abc8755987@gmail.com>
2018-06-26 21:33:12 +0800

    Lab 11: Working on Cycles detection.

--- week11/lab11/MazeCycles.java
+++ week11/lab11/MazeCycles.java
@@ -10,12 +10,39 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private Maze maze;
+    private int[] timesRecord;
+
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        maze.xyTo1D(1, 1);
+        timesRecord = new int[maze.V()];
+    }
+
+    public void detectCycle(int v) {
+        marked[v] = true;
+        timesRecord[v] += 1;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if(timesRecord[w] > 1) {
+                announce();
+                return;
+            }
+
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                detectCycle(w);               
+            }
+        }
     }
 
     @Override
     public void solve() {
+        detectCycle(s);
     }
 } 
 
